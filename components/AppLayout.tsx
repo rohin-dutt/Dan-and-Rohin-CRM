91dfcb8360edb13762e9ba76b3baf9936f772643
Rohin Duttrohin.dutt03@gmail.com <rohin.dutt03@gmail.com>
2026-05-07 12:58:07 -0400

    build phase 1 layout

---
+++ components/AppLayout.tsx
@@ -0,0 +1,10 @@
+import Sidebar from "./Sidebar";
+
+export default function AppLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <main className="flex-1 bg-gray-50 p-8">{children}</main>
+    </div>
+  );
+}
